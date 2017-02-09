f5daa7a34d061947d19606ddc9b72b05008085cc
David Bebb <davidbebb@gmail.com>
2017-02-09 14:44:56 +0000

    update templates to be more parametric, changes date styling to work without social media links

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var ejs = require('ejs');
 var fs = require('fs-extra');
+var moment = require('moment');
 
 var index = fs.readFileSync('./templates/index.ejs', 'utf8');
 
 var author = 'David Bebb';
 var title = 'Caffine to code'
 var data = {
+  lib:{
+    moment:moment
+  },
   header:{
-    title: title + ' | ' + author,
+    title: title + ' by ' + author,
   },
   sidebar:{
     title: title,
@@ -20,19 +24,50 @@ var data = {
       },
       {
         title: 'About Me',
-        href: '#'
+        href: './about.html'
       },
       {
         title: 'Projects',
         href: '#'
       }
     ],
-    recent: []
+    recent: 0 //or an array of links
   },
   pagination:{
     numberOfPages:1,
     currentPage:1
-  }
+  },
+  posts:[
+    {
+      date:'20170206',
+      header:{
+        title:'First Post',
+        subheader: 'This is still my first post.',
+        teaser:'This is the space for click bait',
+      },
+      image:{
+          src: './images/pic02.jpg',
+          alt: 'A pretty picture'
+      },
+      body:[
+        'This is paragraph one',
+        'and <strong>this</strong> is pragraph 2.'
+      ]
+    },
+    {
+      date: '20170228',
+      header:{
+        title:'Second  Post',
+        subheader: 'This is still my first post really.',
+        teaser:'This is the space for click bait'
+      },
+      body:[
+        'Look mom, no picture!',
+        'and <strong>this</strong> is pragraph 2. <- and I still can\'t spell paragraph'
+      ]
+    }
+
+  ]
 }
 
 var html = ejs.render(index, data, {filename:'./templates/index'});
