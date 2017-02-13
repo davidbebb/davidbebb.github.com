12cce9f7b3e88191f4af882c13e0387e2efdf68c
David Bebb <davidbebb@gmail.com>
2017-02-13 17:47:36 +0000

    moved about to sidebar

--- index.js
+++ index.js
@@ -4,35 +4,38 @@ var moment = require('moment');
 
 var index = fs.readFileSync('./templates/index.ejs', 'utf8');
 
-var author = 'David Bebb';
-var title = 'Caffine to code'
+var globals = {
+  author: 'David Bebb',
+  title: 'Caffeine to code'
+}
+
+
 var data = {
-  lib:{
-    moment:moment
-  },
   header:{
-    title: title + ' by ' + author,
+    title: globals.title + ' by ' + globals.author,
   },
   sidebar:{
-    title: title,
-    copyright: ['2015 -', new Date().getFullYear(), author].join(' '),
+    title: globals.title,
+    copyright: ['2015 -', new Date().getFullYear(), globals.author].join(' '),
     pages:[
       {
         title: 'Thoughts',
-        href: '#',
-        class: 'current'
+        href: './index.html'
       },
       {
         title: 'Projects',
         href: './projects.html'
-      },
-      {
-        title: 'About Me',
-        href: './about.html'
+      // },
+      // {
+      //   title: 'About Me',
+      //   href: './about.html'
       }
     ],
     recent: 0 //or an array of links
-  },
+  }
+}
+
+var postsData = {
   pagination:{
     numberOfPages:1,
     currentPage:1
@@ -40,6 +43,7 @@ var data = {
   posts:[
     {
       date:'20170206',
+      tags:[],
       header:{
         title:'First Post',
         subheader: 'This is still my first post.',
@@ -56,6 +60,7 @@ var data = {
     },
     {
       date: '20170228',
+      tags:[],
       header:{
         title:'Second  Post',
         subheader: 'This is still my first post really.',
@@ -70,22 +75,76 @@ var data = {
   ]
 }
 
-var index = ejs.render(index, data, {filename:'./templates/index'});
+var createPage = function (template, data, name){
+  var page = ejs.render(template, data, {filename:'./templates/index'});
+  fs.outputFile(name +'.html', page, function (err) {
+     if (err) {
+       console.log(name, err);
+     } else {
+       console.log('Wrote ' + name);
+     }
+  });
+}
 
-fs.outputFile('index.html', index, function (err) {
- if (err) {console.log(err);}
+function extend(obj, src) {
+  for (var key in src) {
+    if (src.hasOwnProperty(key)) {
+      obj[key] = src[key];
+    }
+  }
+  return obj;
+}
+
+
+var indexData = JSON.parse(JSON.stringify(data));
+var indexData = extend (indexData, postsData);
+indexData.sidebar.pages.find(function (ele){
+  if (ele.title == 'Thoughts') {
+    ele.class = 'current';
+  }
 });
 
 
-var about = ejs.render(index, data, {filename:'./templates/index'});
-
-fs.outputFile('about.html', about, function (err) {
- if (err) {console.log(err);}
+var projectsData = JSON.parse(JSON.stringify(data));
+var projectsData = extend (projectsData, {});
+projectsData.sidebar.pages.find(function (ele){
+  if (ele.title == 'Projects') {
+    ele.class = 'current';
+  }
 });
 
-
-var projects = ejs.render(index, data, {filename:'./templates/index'});
-
-fs.outputFile('projects.html', projects, function (err) {
- if (err) {console.log(err);}
+var aboutData = JSON.parse(JSON.stringify(data));
+var aboutData = extend (aboutData, {
+  about:{}
 });
+aboutData.sidebar.pages.find(function (ele){
+  if (ele.title == 'About Me') {
+    ele.class = 'current';
+  }
+});
+
+var pages = [
+  {
+    template: index,
+    data: indexData,
+    name: 'index'
+  },
+  {
+    template: index,
+    data: aboutData,
+    name: 'about'
+  },
+  {
+    template: index,
+    data: projectsData,
+    name: 'projects'
+  }
+]
+
+
+for (p of pages){
+  p.data.lib = {
+      moment:moment
+    },
+  createPage(p.template, p.data, p.name);
+}
